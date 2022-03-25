dc7b5736058e09d0fbc9e349dfcf82470dec9671
bys1 <bjorn.schonrock@live.nl>
2022-03-25 11:55:27 +0100

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,7 +1,8 @@
 import sys
 from cx_Freeze import setup, Executable
 
-#build_exe_options = {"packages": ["os"], "excludes": ["tkinter"]}
+build_exe_options = {"include_msvcr": True}
+bdist_msi_options = {"target_name": "PASTAQ-GUI.msi"}
 
 # base="Win32GUI" should be used only for Windows GUI app
 #base = None
@@ -12,7 +13,7 @@ setup(
     name="PASTAQ-GUI",
     version="1.0",
     description="GUI for PASTAQ",
-    #options={"build_exe": build_exe_options},
+    options={"build_exe": build_exe_options, "bdist_msi": bdist_msi_options},
     executables=[Executable("src/app.py")],
     install_requires=[
         'pastaq',
