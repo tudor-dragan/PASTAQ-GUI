b9416cd93c33df3472cbbc8fe773acfae2775c6a
Björn Schönrock <bjorn.schonrock@live.nl>
2022-03-29 17:32:38 +0200

    merge

--- src/app.py
+++ src/app.py
@@ -1187,6 +1187,7 @@ class MainWindow(QMainWindow):
 
 # Initialize main window.
 app = QApplication(sys.argv)
+app.setWindowIcon(QIcon(":/icons/pastaq.png"))
 window = MainWindow()
 window.show()
 
