762682351ddc75489d13fbfca863d7a83814b7a6
Björn Schönrock <bjorn.schonrock@live.nl>
2022-05-16 18:34:40 +0200

    CI tests

--- src/tests/test_files.py
+++ src/tests/test_files.py
@@ -2,7 +2,8 @@
 
 import os
 import sys
-from ..files import FileProcessor
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from files import FileProcessor
 from pathlib import Path
 
 file_processor = FileProcessor()
