663408e92307059091175bf6fafb782deed63d39
Björn Schönrock <bjorn.schonrock@live.nl>
2022-05-16 20:47:12 +0200

    files

--- src/tests/test_files.py
+++ src/tests/test_files.py
@@ -3,7 +3,7 @@
 import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from ..files import FileProcessor
+from files import FileProcessor
 from pathlib import Path
 
 file_processor = FileProcessor()
