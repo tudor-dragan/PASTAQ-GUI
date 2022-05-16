fa40191dfe9b1272c1e82596c45a36349e0a2159
Björn Schönrock <bjorn.schonrock@live.nl>
2022-05-16 18:32:03 +0200

    CI tests

---
+++ src/tests/tests_CI.py
@@ -0,0 +1,9 @@
+import test_files as files
+
+print("CI testing suite for PASTAQ-GUI")
+
+print("Testing files...")
+files.tests_CI()
+print("Ran all file tests successfully!")
+
+print("All tests run successfully.")
