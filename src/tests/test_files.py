153fb34483741dda01182143a8b50def1d6bf1f5
Björn Schönrock <bjorn.schonrock@live.nl>
2022-05-18 17:53:41 +0200

    import - corrective maintenance

--- src/tests/test_files.py
+++ src/tests/test_files.py
@@ -1,10 +1,10 @@
 import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-import files
+from files import FileProcessor
 from pathlib import Path
 
-file_processor = files.FileProcessor()
+file_processor = FileProcessor()
 
 def test_check_path():
     filename = 'test.txt'
