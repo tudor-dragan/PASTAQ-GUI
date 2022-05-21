39598e24972608dcb4ea64cbe3869c2a70657693
Björn Schönrock <bjorn.schonrock@live.nl>
2022-05-21 13:52:24 +0200

    params tests

--- src/tests/tests_CI.py
+++ src/tests/tests_CI.py
@@ -1,6 +1,11 @@
+import test_params as params
 import test_files as files
 
 print("CI testing suite for PASTAQ-GUI")
+
+print("Testing parameters...")
+params.tests_CI()
+print("Ran all parameter tests successfully!")
 
 print("Testing files...")
 files.tests_CI()
