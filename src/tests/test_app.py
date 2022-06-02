207222d62ae9bdb1d8c55aca6a1cb40286114073
Drovis <65454390+Drovis@users.noreply.github.com>
2022-06-02 19:37:24 +0200

    app testing v2

--- src/tests/test_app.py
+++ src/tests/test_app.py
@@ -3,7 +3,10 @@ import pytest
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QLabel, QMenuBar, QMenu, QAction, QPushButton, QLineEdit, QWidget
 from PyQt5.QtTest import QTest
+from pathlib import Path
+
 from PyQt5.QtCore import Qt
+import mock
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))  # fixed this for importing app properly
 import app
@@ -12,45 +15,50 @@ import app
 class TestApp:
     application = QApplication(sys.argv)
     mainWindow = app.MainWindow()
-    param = {'instrument_type': 'orbitrap', 'resolution_ms1': 70000,
-             'resolution_msn': 30000,
-             'reference_mz': 200, 'avg_fwhm_rt': 10, 'num_samples_mz': 5,
-             'num_samples_rt': 5,
-             'smoothing_coefficient_mz': 0.4, 'smoothing_coefficient_rt': 0.4,
-             'warp2d_slack': 30,
-             'warp2d_window_size': 100, 'warp2d_num_points': 2000,
-             'warp2d_rt_expand_factor': 0.2,
-             'warp2d_peaks_per_window': 100, 'metamatch_fraction': 0.7,
-             'metamatch_n_sig_mz': 1.5,
-             'metamatch_n_sig_rt': 1.5,
-             'feature_detection_charge_states': [5, 4, 3, 2, 1],
-             'max_peaks': 1000000, 'polarity': 'both', 'min_mz': 0,
-             'max_mz': 100000,
-             'min_rt': 0,
-             'max_rt': 100000, 'link_n_sig_mz': 3, 'link_n_sig_rt': 3,
-             'ident_max_rank_only': True,
-             'ident_require_threshold': True, 'ident_ignore_decoy': True,
-             'similarity_num_peaks': 2000,
-             'qc_plot_palette': 'husl', 'qc_plot_extension': 'png',
-             'qc_plot_fill_alpha': 'dynamic',
-             'qc_plot_line_alpha': 0.5, 'qc_plot_scatter_alpha': 0.3,
-             'qc_plot_scatter_size': 2,
-             'qc_plot_min_dynamic_alpha': 0.1, 'qc_plot_per_file': False,
-             'qc_plot_line_style': 'fill',
-             'qc_plot_dpi': 300, 'qc_plot_font_family': 'sans-serif',
-             'qc_plot_font_size': 7,
-             'qc_plot_fig_size_x': 7.08661, 'qc_plot_fig_size_y': 4.379765814562611,
-             'qc_plot_fig_legend': False,
-             'qc_plot_mz_vs_sigma_mz_max_peaks': 200000,
-             'quant_isotopes': 'height', 'quant_features': 'max_height',
-             'quant_features_charge_state_filter': True,
-             'quant_ident_linkage': 'msms_event',
-             'quant_consensus': True, 'quant_consensus_min_ident': 2,
-             'quant_save_all_annotations': True,
-             'quant_proteins_min_peptides': 1,
-             'quant_proteins_remove_subset_proteins': True,
-             'quant_proteins_ignore_ambiguous_peptides': True,
-             'quant_proteins_quant_type': 'razor'}
+    default_param = {'instrument_type': 'orbitrap', 'resolution_ms1': 70000,
+                     'resolution_msn': 30000,
+                     'reference_mz': 200, 'avg_fwhm_rt': 10, 'num_samples_mz': 5,
+                     'num_samples_rt': 5,
+                     'smoothing_coefficient_mz': 0.4, 'smoothing_coefficient_rt': 0.4,
+                     'warp2d_slack': 30,
+                     'warp2d_window_size': 100, 'warp2d_num_points': 2000,
+                     'warp2d_rt_expand_factor': 0.2,
+                     'warp2d_peaks_per_window': 100, 'metamatch_fraction': 0.7,
+                     'metamatch_n_sig_mz': 1.5,
+                     'metamatch_n_sig_rt': 1.5,
+                     'feature_detection_charge_states': [5, 4, 3, 2, 1],
+                     'max_peaks': 1000000, 'polarity': 'both', 'min_mz': 0,
+                     'max_mz': 100000,
+                     'min_rt': 0,
+                     'max_rt': 100000, 'link_n_sig_mz': 3, 'link_n_sig_rt': 3,
+                     'ident_max_rank_only': True,
+                     'ident_require_threshold': True, 'ident_ignore_decoy': True,
+                     'similarity_num_peaks': 2000,
+                     'qc_plot_palette': 'husl', 'qc_plot_extension': 'png',
+                     'qc_plot_fill_alpha': 'dynamic',
+                     'qc_plot_line_alpha': 0.5, 'qc_plot_scatter_alpha': 0.3,
+                     'qc_plot_scatter_size': 2,
+                     'qc_plot_min_dynamic_alpha': 0.1, 'qc_plot_per_file': False,
+                     'qc_plot_line_style': 'fill',
+                     'qc_plot_dpi': 300, 'qc_plot_font_family': 'sans-serif',
+                     'qc_plot_font_size': 7,
+                     'qc_plot_fig_size_x': 7.08661, 'qc_plot_fig_size_y': 4.379765814562611,
+                     'qc_plot_fig_legend': False,
+                     'qc_plot_mz_vs_sigma_mz_max_peaks': 200000,
+                     'quant_isotopes': 'height', 'quant_features': 'max_height',
+                     'quant_features_charge_state_filter': True,
+                     'quant_ident_linkage': 'msms_event',
+                     'quant_consensus': True, 'quant_consensus_min_ident': 2,
+                     'quant_save_all_annotations': True,
+                     'quant_proteins_min_peptides': 1,
+                     'quant_proteins_remove_subset_proteins': True,
+                     'quant_proteins_ignore_ambiguous_peptides': True,
+                     'quant_proteins_quant_type': 'razor'}
+
+    test_param = {'instrument_type': 'or', 'resolution_ms1': 500,
+                  'resolution_msn': 4,
+                  'reference_mz': 200, 'avg_fwhm_rt': 10, 'num_samples_mz': 5,
+                  'num_samples_rt': 5}
 
     # test if the dark mode is of at initialisation
     def test_init_light_mode(self):
@@ -96,7 +104,7 @@ class TestApp:
     # test the number of actions
     def test_number_actions(self):
         actions = self.mainWindow.findChildren(QAction)
-        assert len(actions) == 11
+        assert len(actions) == 10
 
     # test the number of buttons
     def test_number_btn(self):
@@ -123,11 +131,6 @@ class TestApp:
         buttons = self.mainWindow.findChildren(QAction)
         assert self.mainWindow.reset_param_btn in buttons
 
-    # test is the remove btn is the main window
-    def test_remove_file_btn(self):
-        buttons = self.mainWindow.findChildren(QAction)
-        assert self.mainWindow.remove_file_btn in buttons
-
     # test is the view mode btn is the main window
     def test_view_mode_btn(self):
         buttons = self.mainWindow.findChildren(QAction)
@@ -140,7 +143,7 @@ class TestApp:
 
     # test if the default parameters are the right ones
     def test_default_param(self):
-        assert self.mainWindow.default_param == self.param
+        assert self.mainWindow.default_param == self.default_param
 
     # check if the default parameter have the right length
     def test_len_default_param(self):
@@ -166,30 +169,67 @@ class TestApp:
         lineEdits = self.mainWindow.findChildren(QWidget)
         assert self.mainWindow.project_variables_container in lineEdits
 
-    # check if the update_inst function works
-    def test_update_inst(self): # (not sure if this is relevant)
-        self.param['instrument_type'] = "or"
-        self.param['resolution_ms1'] = 70001
-        self.param['resolution_ms1'] = 30001
-        self.param['reference_mz'] = 201
-        self.param['avg_fwhm_rt'] = 11
-        self.mainWindow.update_inst(params=self.param)
-        assert self.mainWindow.parameters_container.inst_type == self.param['instrument_type']
-        assert self.mainWindow.parameters_container.res_ms1 == self.param['resolution_ms1']
-        assert self.mainWindow.parameters_container.res_ms2 == self.param["resolution_ms1"]
-        assert self.mainWindow.parameters_container.reference_mz == self.param['reference_mz']
-        assert self.mainWindow.parameters_container.avg_fwhm_rt == self.param['avg_fwhm_rt']
+    # check if preparing a new project sets the right parameters and enables the right buttons
+    def test_prepare_new_project(self):
+        self.mainWindow.prepare_new_project('')
+        assert self.mainWindow.parameters_container.parameters == self.default_param
+        assert self.mainWindow.save_project_btn.isEnabled()
+        assert self.mainWindow.save_project_as_btn.isEnabled()
+        assert self.mainWindow.reset_param_btn.isEnabled()
+        assert self.mainWindow.project_variables_container.isEnabled()
+        assert self.mainWindow.parameters_container.isEnabled()
 
-    # # check if the update_inst function works
-    # def test_update_raw(self):
-    #     self.param['instrument_type'] = "or"
-    #     self.param['resolution_ms1'] = 70001
-    #     self.param['resolution_ms1'] = 30001
-    #     self.param['reference_mz'] = 201
-    #     self.param['avg_fwhm_rt'] = 11
-    #     self.mainWindow.update_raw(params=self.param)
-    #     assert self.mainWindow.parameters_container.inst_type == self.param['instrument_type']
-    #     assert self.mainWindow.parameters_container.res_ms1 == self.param['resolution_ms1']
-    #     assert self.mainWindow.parameters_container.res_ms2 == self.param["resolution_ms1"]
-    #     assert self.mainWindow.parameters_container.reference_mz == self.param['reference_mz']
-    #     assert self.mainWindow.parameters_container.avg_fwhm_rt == self.param['avg_fwhm_rt']
+    # check if preparing to open a project sets the right parameters and enables the right buttons
+    def test_prepare_open_project(self):
+        self.mainWindow.parameters_container.parameters = self.default_param
+        self.mainWindow.prepare_open_project(self.test_param, '')
+        assert self.mainWindow.parameters_container.parameters == self.test_param
+        assert self.mainWindow.project_path == ''
+        assert self.mainWindow.save_project_btn.isEnabled()
+        assert self.mainWindow.save_project_as_btn.isEnabled()
+        assert self.mainWindow.reset_param_btn.isEnabled()
+        assert self.mainWindow.project_variables_container.isEnabled()
+        assert self.mainWindow.parameters_container.isEnabled()
+
+    # check if reading the config without config file behaves correctly
+    @mock.patch('app.MainWindow')
+    def test_read_config_without_file(self, mock_window):
+        mock_window.get_config_path.return_value = ''
+        assert mock_window.read_config()
+
+    # TODO: test config with config file (I don't have any - nacer)
+
+    # check if the paths are being loaded when there is no config
+    @mock.patch('app.MainWindow')
+    def test_prepare_paths_tab_without_config(self, mock_window):
+        mock_window.read_config.return_value = False
+        mock_window.prepare_paths_tab()
+        assert not mock_window.parameters_container.load_ms_path.called
+        assert not mock_window.parameters_container.load_id_path.called
+
+    # check if the paths are being loaded when there is a config
+    @mock.patch('app.MainWindow')
+    def test_prepare_paths_tab_with_config(self, mock_window):
+        mock_window.read_config.return_value = './test/'
+        mock_window.prepare_paths_tab()
+        assert mock_window.parameters_container.input_ms.text()
+        assert mock_window.parameters_container.input_id.text()
+
+    # tests if a project saves properly
+    @mock.patch('app.MainWindow')
+    def test_save_project(self, mock_window):
+        mock_window.save_json = True
+        mock_window.save_paths = True
+        self.mainWindow.parameters_container.set_saved(False)
+        mock_window.save_project()
+        assert self.mainWindow.parameters_container.get_saved()
+
+    # test if the run restoration functions properly
+    def test_restore_run(self):
+        self.mainWindow.run_btn.setText('')
+        self.mainWindow.project_variables_container.setEnabled(False)
+        self.mainWindow.parameters_container.setEnabled(False)
+        self.mainWindow.restore_run()
+        assert self.mainWindow.run_btn.text() == 'Run'
+        assert self.mainWindow.project_variables_container.isEnabled()
+        assert self.mainWindow.parameters_container.isEnabled()
